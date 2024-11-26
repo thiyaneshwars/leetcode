39d8fcbd202dbe01f21983f68b422054b483a3ce
thiyaneshwars <138683953+thiyaneshwars@users.noreply.github.com>
2024-11-26 13:58:12 +0530

    Create question3.js

---
+++ question3.js
@@ -0,0 +1,31 @@
+/*3.Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.*/
+
+function trap(height) {
+    let left = 0, right = height.length - 1;
+    let leftMax = 0, rightMax = 0;
+    let waterTrapped = 0;
+
+    while (left < right) {
+        if (height[left] < height[right]) {
+            if (height[left] >= leftMax) {
+                leftMax = height[left];
+            } else {
+                waterTrapped += leftMax - height[left];
+            }
+            left++;
+        } else {
+            if (height[right] >= rightMax) {
+                rightMax = height[right];
+            } else {
+                waterTrapped += rightMax - height[right];
+            }
+            right--;
+        }
+    }
+
+    return waterTrapped;
+}
+
+// Example usage
+console.log(trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1])); // Output: 6
+console.log
